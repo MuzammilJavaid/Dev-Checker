80ef3fc88c583796d821b006faa109b37cd7c6ec
muzammiljavaid <muzammiljavaid89@gmail.com>
2024-08-27 15:26:15 +0500

    Update: check keywords properly

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState([]);
   const [msgPara1, setMsgPara1] = useState("");
   const [msgPara2, setMsgPara2] = useState("");
+  const [checkMsg1, setCheckMsg1] = useState("");
+  const [checkMsg2, setCheckMsg2] = useState("");
 
   const handleChangePara1 = (event) => {
     setMsgPara1(event.target.value);
@@ -19,28 +20,35 @@ function App() {
       return;
     }
 
-    const formattedMessage = formatMessage(msgPara1, msgPara2);
-    setMessage(formattedMessage);
-    setMsgPara1("");
-    setMsgPara2("");
+    const formattedMsg1 = compareWords(msgPara1, msgPara2);
+    const formattedMsg2 = compareWords(msgPara2, msgPara1, false);
+
+    setCheckMsg1(formattedMsg1);
+    setCheckMsg2(formattedMsg2);
   };
 
-  const formatMessage = (para1, para2) => {
-    const maxLength = Math.max(para1.length, para2.length);
+  const compareWords = (para1, para2, isFirst = true) => {
+    const words1 = para1.split(/\s+/);
+    const words2 = para2.split(/\s+/);
+    const maxLength = Math.max(words1.length, words2.length);
     let formattedMsg = [];
 
     for (let i = 0; i < maxLength; i++) {
-      const char1 = para1[i];
-      const char2 = para2[i];
-
-      if (char1 !== char2) {
+      if (words1[i] !== words2[i]) {
         formattedMsg.push(
-          <span key={i} style={{ color: "red", fontWeight: "bold" }}>
-            {char2}
-          </span>
+          <span
+            key={i}
+            style={{
+              backgroundColor: isFirst ? "red" : "green",
+              fontWeight: "bold",
+            }}
+          >
+            {words1[i]}
+          </span>,
+          " "
         );
       } else {
-        formattedMsg.push(<span key={i}>{char2}</span>);
+        formattedMsg.push(<span key={i}>{words1[i]}</span>, " ");
       }
     }
 
@@ -50,7 +58,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-gray-700 flex flex-col justify-center items-center gap-5 px-40">
       <div className="w-full bg-white text-center py-5 rounded-xl">
-        <h1 className="text-3xl font-[600]">Comparing The Paragraphs</h1>
+        <h1 className="text-3xl font-[600]">Diff Checker</h1>
       </div>
       <div className="w-full flex justify-between">
         <div className="flex flex-col items-center gap-4">
@@ -78,12 +86,17 @@ function App() {
       </div>
       <button
         onClick={handleSubmit}
-        className="text-3xl text-white bg-green-500 rounded-3xl p-4 hover:bg-green-700"
+        className="text-3xl text-white bg-green-500 rounded-2xl p-4 hover:bg-green-700"
       >
-        Submit
+        Generate
       </button>
-      <div className="w-full bg-white rounded-lg overflow-auto p-4">
-        <p className="text-xl">{message}</p>
+      <div className="w-full flex justify-between gap-48">
+        <div className="w-full bg-white rounded-lg overflow-auto p-4">
+          <p className="w-full bg-red-200 text-xl">{checkMsg1}</p>
+        </div>
+        <div className="w-full bg-white rounded-lg overflow-auto p-4">
+          <p className="w-full bg-green-200 text-xl">{checkMsg2}</p>
+        </div>
       </div>
     </div>
   );
